a26f111e69d4e4c613fbb2b73cbce851b639a57e
Patryk Kożuch <patryk.kozuch.1@gmail.com>
2025-03-27 23:54:49 +0100

    Add BUPG Controller

--- gupb/together_config.py
+++ gupb/together_config.py
@@ -1,16 +1,18 @@
 from gupb.controller import random
+from gupb.controller.bupg import bupg
 from gupb.scripts import arena_generator
 
 CONFIGURATION = {
     'arenas': arena_generator.generate_arenas(10, arena_generator.random_size_generator()),
     'controllers': [
+        bupg.BUPGController("BUPG Minion"),
         random.RandomController("Alice"),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius"),
     ],
     'start_balancing': False,
-    'visualise': False,
+    'visualise': True,
     'show_sight': False,
-    'runs_no': 100,
+    'runs_no': 1,
 }
--- gupb/view/render.py
+++ gupb/view/render.py
@@ -74,6 +74,7 @@ class SpriteRepository:
             characters.Tabard.VIOLET: load_sprite('characters', 'champion_violet', BLACK),
             characters.Tabard.WHITE: load_sprite('characters', 'champion_white', BLACK),
             characters.Tabard.YELLOW: load_sprite('characters', 'champion_yellow', BLACK),
+            characters.Tabard.MINION: load_sprite('characters', 'minion', None),
 
             effects.Mist: load_sprite('effects', 'mist', BLACK),
             effects.WeaponCut: load_sprite('effects', 'blood', BLACK),
@@ -102,6 +103,7 @@ class SpriteRepository:
                     characters.Tabard.VIOLET,
                     characters.Tabard.WHITE,
                     characters.Tabard.YELLOW,
+                    characters.Tabard.MINION,
                 ],
                 [
                     characters.Facing.RIGHT,
