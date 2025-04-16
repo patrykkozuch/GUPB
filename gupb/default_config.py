8fc764efff9603e9577d5d62638410af99b47201
IgorSwat <114943112+IgorSwat@users.noreply.github.com>
2025-04-16 22:20:41 +0200

    norgul bot initial commit (#334)
    * norgul bot initial commit
    
    * Norgul is growing...
    
    * lol
    
    * lol2
    
    * Jakub Krewetczyk powstaje
    
    * More fixes
    
    * More fixes
    
    * Wojtyła is now prioritizing to spin on obelisk
    
    * Added manhattan dist function
    
    * Added option to move without turning and created move_to_target func
    
    * Norgul gotowy do walki
    
    * Norgul ist bereit zu kämpfen
    
    ---------
    
    Co-authored-by: Jakub Kasperski <k2002kuba@gmail.com>
    Co-authored-by: Radosław Łazarz <radek.lazarz@gmail.com>

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,5 +1,6 @@
 from gupb.controller import keyboard
 from gupb.controller import random
+from gupb.controller import norgul
 from gupb.controller import kirby
 from gupb.controller.neat.kim_dzong_neat_jr import KimDzongNeatJuniorController
 
@@ -16,6 +17,7 @@ CONFIGURATION = {
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius"),
+        norgul.NorgulController("Norgul")
         kirby.KirbyController("Kirby")
         KimDzongNeatJuniorController(),
     ],
